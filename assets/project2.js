8866b07c7f0ed4ef2177a457697193174654d7fc
Shardul Vaidya <cam.v737@gmail.com>
2022-07-17 19:45:37 -0400

    Added in TODOs

--- assets/project2.js
+++ assets/project2.js
@@ -3,6 +3,26 @@
  */
 // defining the base distance for each side of the program
 // can be used for a bunch of functions including calculating the div background locations
+
+
+/*
+    Shardul - TODO:
+        * Finish making the tiles interactible
+        * Get timer working - 1 feature (game time + music)
+        * Finish UI builder - 1 feature (different puzzles sizes)
+        * Add in more backgrounds - 1 feature (multiple backgrounds)
+        * Multi-select on hover - 1 feature (multi-select)
+
+
+    *** FIGURE OUT EXTRA FEATURE??? - Second scale of difficulty (all white/all black tiles)??? ***
+ */
+
+/*
+    Ruchita - TODO:
+        * Finish Shuffler
+        * Finish Solver - 1 Feature (solver)
+        * End of game notification - 1 Feature (EoG noti)
+*/
 const BASE_DISTANCE = 100;
 
 // helper functions
